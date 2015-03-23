f2891dd0d51b59cbb257e9b4ae14d7438ebd0846
Matt Reider <mreider@gmail.com>
2015-03-23 12:23:48 -0700

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -1,6 +1,6 @@
 OPS_MGR_PASSWD=$1
 mkdir -p decrypted 2>/dev/null
-cd t1
+cd decrypted
 ruby ../eos.rb encrypt $OPS_MGR_PASSWD decrypted-installation.yml ../installation.yml
 ruby ../eos.rb encrypt $OPS_MGR_PASSWD decrypted-actual-installation.yml ../actual-installation.yml
 cd ..
