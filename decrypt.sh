d40c05c9bb72136ce8ac29ea333579efd9ef26f2
Stuart Charlton <scharlton@pivotal.io>
2016-05-30 14:57:12 -0600

    Update encrypt/decrypt scripts for Ops Manager 1.7 Compatibility with 1.6 and earlier is retained

--- decrypt.sh
+++ decrypt.sh
@@ -1,6 +1,13 @@
 OPS_MGR_PASSWD=$1
-mkdir -p decrypted 2>/dev/null
+sudo -u tempest-web mkdir -p decrypted 2>/dev/null
+chown tempest-web -R decrypted
 cd decrypted
-ruby ../eos.rb decrypt $OPS_MGR_PASSWD ../installation.yml decrypted-installation.yml
-ruby ../eos.rb decrypt $OPS_MGR_PASSWD ../actual-installation.yml decrypted-actual-installation.yml
+if [ -f /home/tempest-web/tempest/web/scripts/decrypt ];
+then
+  sudo -u tempest-web RAILS_ENV=production /home/tempest-web/tempest/web/scripts/decrypt $OPS_MGR_PASSWD ../installation.yml decrypted-installation.yml
+  sudo -u tempest-web RAILS_ENV=production /home/tempest-web/tempest/web/scripts/decrypt $OPS_MGR_PASSWD ../actual-installation.yml decrypted-actual-installation.yml
+else 
+  ruby ../eos.rb decrypt $OPS_MGR_PASSWD ../installation.yml decrypted-installation.yml
+  ruby ../eos.rb decrypt $OPS_MGR_PASSWD ../actual-installation.yml decrypted-actual-installation.yml
+fi
 cd ..
